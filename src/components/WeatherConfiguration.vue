885330e9a0c4eb1727cf2e64305b3c6a29f16da4
nod696 <tremor696@rambler.ru>
2023-02-06 22:19:21 +0500

    good

---
+++ src/components/WeatherConfiguration.vue
@@ -0,0 +1,190 @@
+<template>
+
+<div class="widget__config" v-if="!props.isClosed">
+    <h3 class="widget__config-title">Settings</h3>
+    <div class="weather__configuration">
+    <draggable
+      v-model="citiesArray"
+      handle=".weather__configuration__item-hamburger"
+      @start="drag = true"
+      @end="drag = false"
+      item-key="id"
+    >
+      <template #item="{ element }">
+        <div class="weather__configuration__item">
+          <button class="weather__configuration__item-hamburger"></button>
+          <span class="weather__configuration__item-city"
+            >{{ element.name }},
+            {{ element.country ? element.country : "Ne" }}</span
+          >
+          <button
+            class="weather__configuration__item-delete"
+            @click="deleteCity(element)"
+          >
+            <img
+              class="weather__configuration__item-delete-img"
+              src="../assets/delete.png"
+              alt=""
+            />
+          </button>
+        </div>
+      </template>
+    </draggable>
+    <div class="weather__configuration__location">
+      <label class="weather__configuration__location-label" for="input"
+        >Add Location:       <input
+        type="text"
+        class="weather__configuration__location-input"
+        id="input"
+        v-model="inputCity"
+      />
+      </label>
+
+      <button
+        class="weather__configuration__location-button"
+        @click="addCity"
+      >
+        <img class="weather__configuration__location-button-icon" src="../assets/add.png" alt="">
+      </button>
+    </div>
+  </div>
+</div>
+
+</template>
+
+
+<script setup>
+
+import {defineProps, ref, onMounted, defineEmits, watchEffect} from "vue";
+import draggable from "vuedraggable";
+import {useFetchData} from "../composables/useFetchData";
+let props = defineProps(['cities', 'isClosed']);
+let citiesArray = ref(props.cities);
+
+let inputCity = ref("");
+
+let emit = defineEmits("changeData");
+
+
+let drag = ref(false);
+let config = ref(null);
+
+let addCity = async () => {
+    let {weatherCityInfo} = await useFetchData(inputCity.value);
+    citiesArray.value = [...citiesArray.value,...weatherCityInfo.value];
+
+};
+
+let deleteCity = (element) => {
+  citiesArray.value = citiesArray.value.filter((city, index) => {
+    return city !== element;
+  });
+};
+
+
+watchEffect(() => {
+    emit('changeData', citiesArray.value);
+})
+</script>
+
+<style>
+.widget__config{
+  width: 100%;
+  height: 300px;
+  position: absolute;
+  top: 0;
+  -webkit-box-shadow: 0px 6px 8px 0px rgba(34, 60, 80, 0.2);
+-moz-box-shadow: 0px 6px 8px 0px rgba(34, 60, 80, 0.2);
+box-shadow: 0px 6px 8px 0px rgba(34, 60, 80, 0.2);
+
+}
+.weather__configuration {
+    padding-top: 50px;
+  background-color: rgb(189, 228, 247);
+  width: 100%;
+  height: 80%;
+  transition: transform .2s linear;
+}
+
+.weather__configuration__item {
+  background-color: rgb(160, 196, 226);
+  -webkit-box-shadow: 0px 6px 8px 0px rgba(34, 60, 80, 0.2);
+-moz-box-shadow: 0px 6px 8px 0px rgba(34, 60, 80, 0.2);
+box-shadow: 0px 6px 8px 0px rgba(34, 60, 80, 0.2);
+  color: white;
+  margin: 0 auto;
+  padding: 10px;
+  width: 200px;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+
+
+.weather__configuration__item-hamburger {
+    position: relative;
+  cursor: pointer;
+  width: 20px;
+    height: 20px;
+    background-color: transparent;
+    border: none;
+    border-top: 2px solid black;
+}
+.weather__configuration__item-hamburger:after{
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 100%;
+    width: 20px;
+    height: 3px;
+    background-color: black;
+}
+.weather__configuration__item-hamburger:before{
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 20px;
+    height: 3px;
+    background-color: black;
+
+}
+.weather__configuration-header {
+  display: flex;
+  justify-content: space-between;
+}
+
+.weather__configuration__location-label {
+  display: block;
+}
+
+.weather__configuration__item-delete {
+  background-color: transparent;
+  outline: none;
+  border: none;
+  cursor: pointer;
+}
+.weather__configuration__item-delete-img {
+  width: 20px;
+  height: 20px;
+}
+.weather__configuration__location{
+    margin-top: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.weather__configuration__location-button{
+    cursor: pointer;
+    background-color: transparent;
+    border: none;
+
+}
+.weather__configuration__location-button-icon{
+    width: 25px;
+    height: 25px;
+}
+</style>
--- src/components/WeatherWidget.vue
+++ src/components/WeatherWidget.vue
@@ -6,11 +6,12 @@
             :src="isClosed ? './gear.png' : './close.png'"
             alt=""
           />
-        </button>
-    <div class="widget__item" v-for="city in weatherItems" :key="city">
+    </button>
+    <WeatherConfiguration v-if="weatherItems" @changeData="changeDataCities" :cities="weatherItems" :isClosed="isClosed"></WeatherConfiguration>
+    <div class="widget__list" v-if="isClosed">
+      <div class="widget__item" v-for="city in weatherItems" :key="city">
       <header class="widget__header">
-        <h3 class="title">{{ isClosed ? city.name : "Settings" }}</h3>
-
+        <h3 class="title">{{ city.name }}</h3>
       </header>
       <div class="widget__item-representaion" v-if="city.weatherData">
         <img
@@ -43,116 +44,41 @@
           }}m
         </p>
         </div>
-
-
         </div>
       </div>
-
-    <div class="weather__configuration" ref="config">
-      <draggable
-        v-model="weatherItems"
-        handle=".weather__configuration__item-hamburger"
-        @start="drag = true"
-        @end="drag = false"
-        item-key="id"
-      >
-        <template #item="{ element }">
-          <div class="weather__configuration__item">
-            <button class="weather__configuration__item-hamburger">Ham</button>
-            <span class="weather__configuration__item-city"
-              >{{ element.name }},
-              {{ element.country ? element.country : "Ne" }}</span
-            >
-            <button
-              class="weather__configuration__item-delete"
-              @click="deleteCity(element)"
-            >
-              <img
-                class="weather__configuration__item-delete-img"
-                src="../assets/delete.png"
-                alt=""
-              />
-            </button>
-          </div>
-        </template>
-      </draggable>
-      <div class="weather__configuration__location">
-        <label class="weather__configuration__location-label" for="input"
-          >Add Location:
-        </label>
-        <input
-          type="text"
-          class="weather__configuration__location-input"
-          id="input"
-          v-model="inputCity"
-        />
-        <button
-          class="weather__configuration__location-button"
-          @click="addCity"
-        >
-          Add
-        </button>
-      </div>
     </div>
+
   </section>
+
 </template>
 
 <script setup>
 
 import { ref, onMounted } from "vue";
+import {useFetchData} from "../composables/useFetchData";
+import WeatherConfiguration from "./WeatherConfiguration.vue";
 
-import draggable from "vuedraggable";
+let weatherItems = ref();
 
-let inputCity = ref("");
 
-let drag = ref(false);
-let config = ref(null);
-
-let addCity = () => {
-  getData(inputCity.value);
-  saveCities();
-};
-
-let deleteCity = (element) => {
-  weatherItems.value = weatherItems.value.filter((city, index) => {
-    return city !== element;
-  });
-};
-
-const API_KEY = "06b132db1e926d7c73ce52431bc7a68c";
-
-let weatherIcon = ref("");
-let isClosed = ref(true);
-let weatherItems = ref([]);
-async function getData(city) {
-  let result = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-    );
-    let data = await result.json();
-    weatherItems.value.push({
-      name: city,
-      country: data.sys.country,
-      weatherData: data,
-      weatherIcon: data.weather[0].icon,
-    })
+function changeDataCities(arr){
+  weatherItems.value = arr;
 }
+
+let isClosed = ref(true);
 function openConfig() {
   isClosed.value = !isClosed.value;
-  isClosed.value ?  config.value.className=config.value.className = "weather__configuration": config.value.className=config.value.className + " active" ;
-  console.log(config.value.className);
+
 
 }
-
-function saveCities(){
-  localStorage.setItem("cities", weatherItems.value);
-}
-
-
 onMounted(async () => {
   navigator.geolocation.getCurrentPosition(async(position) => {
     let cityData = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${position.coords.latitude}&longitude=${position.coords.longitude}&localityLanguage=en`);
     let {city} = await cityData.json();
-    getData(city);
+    let {weatherCityInfo} = await useFetchData(city);
+    weatherItems.value = [];
+    weatherItems.value = [...weatherItems.value, ...weatherCityInfo.value];
+    return;
   });
 });
 </script>
@@ -164,6 +90,7 @@ $title-size: 24px;
 
 .title{
   font-size: $title-size;
+  padding: 0 0 0 20px;
 }
 
 
@@ -173,10 +100,13 @@ $title-size: 24px;
   justify-content: center;
 }
 .widget__container {
+  position: relative;
   max-width: 400px;
   margin: 0 auto;
-  border: 1px solid black;
-  position: relative;
+  -webkit-box-shadow: 0px 6px 8px 0px rgba(34, 60, 80, 0.2);
+-moz-box-shadow: 0px 6px 8px 0px rgba(34, 60, 80, 0.2);
+box-shadow: 0px 6px 8px 0px rgba(34, 60, 80, 0.2);
+
 }
 
 .widget__header {
@@ -185,10 +115,13 @@ $title-size: 24px;
   height: 20%;
 }
 .configuration__button {
+  cursor: pointer;
   position: absolute;
+  z-index: 10;
   right: 0;
-  width: 50px;
-  height: 50px;
+  width: 35px;
+  height: 35px;
+  margin: 20px;
   outline: none;
   border: none;
   background-color: transparent;
@@ -200,8 +133,8 @@ $title-size: 24px;
   font-size: $title-size * 1.2;
 }
 .configuration__button-img {
-  width: 50px;
-  height: 50px;
+  width: 35px;
+  height: 35px;
 }
 .widget__item-icon {
   width: 100px;
@@ -216,54 +149,5 @@ $title-size: 24px;
   display: grid;
   grid-template: 1fr 1fr / 1fr 1fr;
 }
-.weather__configuration {
-  border: 1px solid black;
-  position: absolute;
-  left: 0;
-  top: 0;
-  background-color: aqua;
-  width: 100%;
-  height: 80%;
-  transform: translateY(-200%);
-  transition: transform .2s linear;
-}
 
-.weather__configuration.active {
-  transform: translateY(20%);
-}
-.weather__configuration__item {
-  background-color: rgb(160, 196, 226);
-  border: 1px solid black;
-  color: white;
-  margin: 0 auto;
-  width: 200px;
-  height: 100px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.weather__configuration__item-hamburger {
-  cursor: pointer;
-}
-
-.weather__configuration-header {
-  display: flex;
-  justify-content: space-between;
-}
-
-.weather__configuration__location-label {
-  display: block;
-}
-
-.weather__configuration__item-delete {
-  background-color: transparent;
-  outline: none;
-  border: none;
-  cursor: pointer;
-}
-.weather__configuration__item-delete-img {
-  width: 20px;
-  height: 20px;
-}
 </style>
